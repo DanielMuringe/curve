134551a83d1bddcf77e897622bb2622ad149ec10
captain <danielndungumuringe@gmail.com>
2022-09-21 11:31:38 +0300

    Made <class 'plotted_curve'> verbose

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from numpy.linalg import solve as np_linalg_solve
 from numpy import array as np_array
-
-
+from ..essentials.string import join as str_join
 
 class plotted_curve():
     def __init__(self,points):
-        self.points = points
+        self.points = np_array(points)
     
-    def eq(self):
+    def constants(self):
         A = []
         B = []
 
@@ -24,4 +23,33 @@ class plotted_curve():
         B = np_array(B)
 
         X = np_linalg_solve(A,B)
+
         return X
+    
+    def __str__(self):
+        superscript = []
+        line        = []
+
+        i = 0
+        for constant in self.constants():
+            constant    = round(constant,2)
+            cond        = i<=0 or constant<0
+            plus_space  = '' if cond else ' '
+            plus        = '' if cond else '+'
+            const_space = ' '*(len(str(constant)))
+
+            print(f'{constant}->{cond}')
+            
+            superscript.append(f'{plus_space}{const_space} {i}')
+            line.append(f'{plus}{constant}x ')
+
+            i += 1
+            
+        superscript = str_join(superscript)
+        line        = str_join(line)
+        
+        eq_str = f'''
+{superscript}
+{line}'''
+        return eq_str
+
